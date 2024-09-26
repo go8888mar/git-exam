75e4c0517606a5dfa0ba782c38145bd884bc7979
go8888mar <go8888mar@naver.com>
2024-09-26 11:32:19 +0900

    commit all

---
+++ test01.py
@@ -0,0 +1,11 @@
+#test01.py
+def addNum(a, b):
+    sum = a + b
+    return sum
+
+num01 = 100 #수정
+num02 = 20
+
+print(addNum(num01, num02))
+
+print("Program over") # 추가
---
+++ test03.py
@@ -0,0 +1,8 @@
+#test03.py
+
+def sayHello(name):
+    return "Hello, " + name
+
+inName = "Tom"
+print(sayHello(inName))
+print("add")
